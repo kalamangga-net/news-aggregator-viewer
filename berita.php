6e75a007169e87dddd97ffc47dd58a5bc9c4af40
Yudha H Tejaningrat <yudha_ht@yahoo.com>
2016-11-13 21:52:03 +0700

    news: show sources

--- berita.php
+++ berita.php
@@ -3,13 +3,14 @@
 $p = 20;
 if($_GET['page']) {
  $x = (strval($_GET['page']) - 1);
- $hal = ($p * $x).", ".$p; 
+ $hal = ($p * $x).", ".$p;
 } else {
  $hal = "0, ".$p;
 };
 
-$query = "SELECT id,judul,ringkasan,tanggal FROM link ORDER BY id DESC LIMIT ".$hal;
+$query = "SELECT l.id, l.judul, l.tanggal, x.nama, x.url FROM link l LEFT JOIN xml x ON x.id=l.id_sindikasi ORDER BY id DESC LIMIT ".$hal;
 foreach ($db->iterate($query) as $row) {
+/*
 	$text = preg_replace("/<img[^>]+\>/i", "", $row->ringkasan);
 	$text = preg_replace("/<div[^>]+\>/i", "", $text);
         $text = preg_replace("/<table[^>]+\>/i", "", $text);
@@ -17,11 +18,10 @@ foreach ($db->iterate($query) as $row) {
         $text = preg_replace("/<td[^>]+\>/i", "", $text);
         $text = preg_replace("/<\/tr>/i", "", $text);
         $text = preg_replace("/<\/td>/i", "", $text);
-//        $text = preg_replace("/<\/a>/i", "Link</a>", $text);
+        $text = preg_replace("/<\/a>/i", "Link</a>", $text);
         $text = preg_replace("/<\/table>/i", "", $text);
         $text = preg_replace("/<\/div>/i", "", $text);
-  
-  	$text = preg_replace("(http://)", "http://adf.ly/238753/banner/", $text);
+  	$text = preg_replace("(http://)", "http://adf.ly/238753/banner/", $text); */
 	$link = str_replace(' ', '-', $row->judul);
 	$link = str_replace(',', '', $link);
 	$link = str_replace('&', '', $link);
@@ -31,7 +31,7 @@ foreach ($db->iterate($query) as $row) {
 	$link = str_replace('!', '', $link);
 	$link = str_replace('--', '-', $link);
 	$link = strtolower($link);
-	$news .= "<p>".$row->tanggal." - <a href='/news/".$row->id."/".$link."'>".$row->judul."</a></p>\n";
+	$news .= "<p>".$row->tanggal." - <a href='".$row->url."'>".$row->nama."</a>, <a href='/news/".$row->id."/".$link."'>".$row->judul."</a></p>\n";
 }
 
 ?>
