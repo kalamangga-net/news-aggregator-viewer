ca47446642895cfcdf2b421ab6fa57050115a13d
Yudha H Tejaningrat <yudha_ht@yahoo.com>
2019-11-05 14:08:06 +0700

    [pustaka] Quixotix-PHP-MySQL: mysql->mysqli refix

--- pustaka/Quixotix-PHP-MySQL/mysqldatabase.php
+++ pustaka/Quixotix-PHP-MySQL/mysqldatabase.php
@@ -7,20 +7,20 @@
  *  MySQL Database
  *
  *  A singleton object which provides convenience methods for interfacing with
- *  a MySQL database in PHP 5. You can get the object's instance using the 
+ *  a MySQL database in PHP 5. You can get the object's instance using the
  *  static {@link getInstance()} method. Being a singleton object, this class
  *  only supports one open database connection at a time and idealy suited to
- *  single-threaded applications. You can read 
- *  about {@link http://php.net/manual/en/language.oop5.patterns.php the singleton 
+ *  single-threaded applications. You can read
+ *  about {@link http://php.net/manual/en/language.oop5.patterns.php the singleton
  *  pattern in the PHP manual}.
- *  
+ *
  *  <b>Getting Started</b>
  *  <code>
  *  $db = MySqlDatabase::getInstance();
- *  
+ *
  *  try {
  *      $db->connect('localhost', 'user', 'password', 'database_name');
- *  } 
+ *  }
  *  catch (Exception $e) {
  *      die($e->getMessage());
  *  }
@@ -40,24 +40,24 @@ class MySqlDatabase
      *  @var resource
      */
     public $link;
-    
+
     /**
      *  @var string
      */
     private $conn_str;
-    
+
     /**
      *  @var MySqlDatabase
      */
     private static $instance;
-    
+
     const MYSQL_DATE_FORMAT = 'Y-m-d';
     const MYSQL_TIME_FORMAT = 'H:i:s';
     const MYSQL_DATETIME_FORMAT = 'Y-m-d H:i:s';
-    
+
     const INSERT_GET_AUTO_INCREMENT_ID = 1;
     const INSERT_GET_AFFECTED_ROWS = 2;
-    
+
     /**
      *  Constructor
      *
@@ -68,7 +68,7 @@ class MySqlDatabase
     /**
      *  Connect
      *
-     *  Establish a connection to a MySQL database. Returns the MySQL link 
+     *  Establish a connection to a MySQL database. Returns the MySQL link
      *  link identifier or throws an exception if there is an error.
      *
      *  <code>
@@ -78,7 +78,7 @@ class MySqlDatabase
      *  // connect to a MySQL database (use your own login information)
      *  try {
      *      $db->connect('localhost', 'user', 'password', 'database_name');
-     *  } 
+     *  }
      *  catch (Exception $e) {
      *      die($e->getMessage());
      *  }
@@ -98,21 +98,21 @@ class MySqlDatabase
         } else {
             $this->link = @mysqli_connect($host, $user, $password);
         }
-        
-        if (!$this->link) 
+
+        if (!$this->link)
         {
-            throw new Exception('Unable to establish database connection: ' 
-                                .mysql_error());
+            throw new Exception('Unable to establish database connection: '
+                                .mysqli_error());
         }
 
         if ($database) $this->useDatabase($database);
-        
+
         $version = mysql_get_server_info();
         $this->conn_str = "'$database' on '$user@$host' (MySQL $version)";
-        
+
         return $this->link;
     }
-    
+
     /**
      *  Delete
      *
@@ -127,16 +127,16 @@ class MySqlDatabase
      *  @param  string
      *  @return integer
      */
-    public function delete($query) 
+    public function delete($query)
     {
         return $this->updateOrDelete($query);
     }
-    
+
     /**
      *  Get Connection String
      *
      *  Gets a string representing the connection.
-     *  
+     *
      *  <code>
      *  echo $db->getConnectionString();
      *  // example output: 'test_database' on 'web_user@localhost' (MySQL 5.1.47)
@@ -144,11 +144,11 @@ class MySqlDatabase
      *
      *  @return string
      */
-    public function getConnectionString() 
+    public function getConnectionString()
     {
         return $this->conn_str;
     }
-    
+
     /**
      *  Get Instance
      *
@@ -167,20 +167,20 @@ class MySqlDatabase
         {
             self::$instance = new MySqlDatabase();
         }
-        
+
         return self::$instance;
     }
-    
+
     /**
      *  Fetch One From Each Row
-     *  
+     *
      *  Convenience method to get a single value from every row in a given
      *  query. This is usefull in situations where you know that the result will
-     *  only have only one column of data and you need that all in a simple 
+     *  only have only one column of data and you need that all in a simple
      *  array.
      *
      *  <code>
-     *  
+     *
      *  $query = "SELECT name FROM users";
      *  $names = $db->fetchOneFromEachRow($query);
      *  echo 'Users: ' . implode(', ', $names);
@@ -192,24 +192,24 @@ class MySqlDatabase
     public function fetchOneFromEachRow($query)
     {
         $rval = array();
-        
+
         foreach ($this->iterate($query, MySqlResultSet::DATA_NUMERIC_ARRAY) as $row) {
             $rval[] = $row[0];
         }
 
         return $rval;
     }
-    
+
     /**
      *  Fetch One Row
-     *  
-     *  Convenience method to get a single row from a given query. This is 
-     *  usefull in situations where you know that the result will only contain
-     *  one record and therefore do not need to iterate over it. 
      *
-     *  You can 
-     *  optionally  specify the type of data to be returned (object or array) 
-     *  using one of the MySqlResultSet Data Constants. The default is 
+     *  Convenience method to get a single row from a given query. This is
+     *  usefull in situations where you know that the result will only contain
+     *  one record and therefore do not need to iterate over it.
+     *
+     *  You can
+     *  optionally  specify the type of data to be returned (object or array)
+     *  using one of the MySqlResultSet Data Constants. The default is
      *  {@link MySqlResultSet::DATA_OBJECT}.
      *
      *  <code>
@@ -217,7 +217,7 @@ class MySqlDatabase
      *  $query = "SELECT first, last FROM users WHERE user_id = 24 LIMIT 1";
      *  $row = $db->fetchOneRow($query);
      *  echo $row->foo;
-     *  echo $row->bar; 
+     *  echo $row->bar;
      *  </code>
      *
      *  @param  string
@@ -232,10 +232,10 @@ class MySqlDatabase
 
         return $row;
     }
-    
+
     /**
      *  Fetch One
-     *  
+     *
      *  Convenience method to get a single value from a single row. Returns the
      *  value if the query returned a record, false if there were no results, or
      *  throws an exception if there was an error with the query.
@@ -250,7 +250,7 @@ class MySqlDatabase
      */
     public function fetchOne($query)
     {
-        $result = new MySqlResultSet($query, MySqlResultSet::DATA_NUMERIC_ARRAY, 
+        $result = new MySqlResultSet($query, MySqlResultSet::DATA_NUMERIC_ARRAY,
                                      $this->link);
         $result->rewind();
         $row = $result->current();
@@ -258,29 +258,29 @@ class MySqlDatabase
         if (!$row) return false;
         else return $row[0];
     }
-    
+
     /**
      *  Import SQL File
      *
-     *  Runs the queries defined in an SQL script file. The double-hyphen style 
+     *  Runs the queries defined in an SQL script file. The double-hyphen style
      *  comments must have a single space after the hyphens. Hash style comments
      *  and C-style comments are also supported.
      *
-     *  An optional user callback function can be specified to get information 
+     *  An optional user callback function can be specified to get information
      *  about each MySQL statement. The user callback function takes 3
-     *  parameters: the line number as an integer, the query as a string, and the 
+     *  parameters: the line number as an integer, the query as a string, and the
      *  result of the query as a boolean.
      *
      *  <code>
-     *  function import_sql_callback($line_number, $sql_query, $result) 
+     *  function import_sql_callback($line_number, $sql_query, $result)
      *  {
      *      echo "Line $line_number: $sql_query ";
      *      if ($result) echo "(OK)<br/>";
-     *      else echo "(FAIL)<br/>";    
+     *      else echo "(FAIL)<br/>";
      *  }
      *  </code>
      *
-     *  You can optionally specify whether or not to abort importing statements 
+     *  You can optionally specify whether or not to abort importing statements
      *  when an SQL error occurs (defaults to 'true') in which case an exception
      *  will be thrown for any MySQL error.
      *
@@ -309,49 +309,49 @@ class MySqlDatabase
         }
 
         $lines = $this->loadFile($filename);
-        
+
         $num_queries = 0;
         $sql_line = 0;
         $sql = '';
         $in_comment = false;
-        
+
         foreach ($lines as $num => $line) {
-            
+
             $line = trim($line);
             $num++;
             if (empty($sql)) $sql_line = $num;
-            
+
             // ignore comments
-            
+
             if ($in_comment) {
                 $comment = strpos($line, '*/');
-                
+
                 if ($comment !== false) {
                     $in_comment = false;
                     $line = substr($line, $comment+2);
                 } else {
                     continue;
                 }
-                
+
             } else {
-                
+
                 $comment = strpos($line, '/*');
-                
+
                 if ($comment !== false) {
-                    
+
                     if (strpos($line, '*/') === false) {
                         $in_comment = true;
                     }
-                    
+
                     $line = substr($line, 0, $comment);
-                    
+
                 } else {
-                
+
                     // single line comments
-                    
+
                     foreach (array('-- ', '#') as $chars) {
                         $comment = strpos($line, $chars);
-                        
+
                         if ($comment !== false) {
                             $line = substr($line, 0, $comment);
                         }
@@ -360,34 +360,34 @@ class MySqlDatabase
             }
 
             // check if the statement is ready to be queried
-            
+
             $end = strpos($line, ';');
-            
+
             if ($end === false) {
                 $sql .= $line;
             } else {
                 $sql .= substr($line, 0, $end);
                 $result = $this->quickQuery($sql);
                 $num_queries++;
-                
+
                 if (!$result && $abort_on_error) {
                     $file = basename($filename);
                     $error = mysqli_error($this->link);
                     throw new Exception("Error in $file on line $sql_line: $error");
                 }
-                
+
                 if ($callback) {
                     call_user_func($callback, $sql_line, $sql, $result);
                 }
-                
+
                 $sql = '';  // clear for next statement
-                
+
             }
         }
-        
+
         return $num_queries;
     }
-    
+
     /**
      *  Is Connected
      *
@@ -403,7 +403,7 @@ class MySqlDatabase
             return false;
         }
     }
-    
+
     // insertPhpArray
     // insertSqlArray
     // sqlval()
@@ -428,17 +428,17 @@ class MySqlDatabase
      *  @param  integer
      *  @return integer
      */
-    public function insert($query, $r_type=MySqlDatabase::INSERT_GET_AUTO_INCREMENT_ID) 
+    public function insert($query, $r_type=MySqlDatabase::INSERT_GET_AUTO_INCREMENT_ID)
     {
         $r = $this->query($query);
-        
+
         if ($r_type == MySqlDatabase::INSERT_GET_AFFECTED_ROWS) {
             return @mysqli_affected_rows($this->link);
         } else {
             return @mysqli_insert_id($this->link);
         }
     }
-    
+
     /**
      *  DO NOT USE
      *
@@ -455,15 +455,15 @@ class MySqlDatabase
         if (empty($table) || !is_string($table)) {
             throw new Exception('The $table parameter must be specified as a string.');
         }
-        
+
         $table_sql = '`' . @mysqli_real_escape_string($table) . '`';
         $query = "INSERT INTO $table_sql ";
-        
+
         // columns
         if (is_string($columns)) {
             $columns = explode(',', $columns);
         }
-        
+
         if (is_array($columns)) {
             foreach ($columns as &$col) {
                 if (!is_string($col)) {
@@ -476,27 +476,27 @@ class MySqlDatabase
         } else {
             throw new Exception('The $columns parameter must be a string or an array of strings.');
         }
-        
+
         try {
             $column_info = array();
-            
+
             foreach ($this->iterate("SHOW COLUMNS FROM $table_sql") as $row) {
                 $column_info[] = $row;
             }
-        } 
+        }
         catch (Exception $e) {
             throw new Exception("Could not get column information for table $table_sql.");
         }
-        
+
         $query .= "($column_sql) ";
-        
+
         // values
-        
+
         if (is_array($values)) {
             for ($i=0; $i < count($values); $i++) {
                 $info = $column_info[$i];
                 $value = $values[i];
-                
+
                 // Where the heck did I leave off?
             }
         } else {
@@ -504,21 +504,21 @@ class MySqlDatabase
 
             throw new Exception('The $values parameter must be a string or an array.');
         }
-        
+
         if (isset($column_count) && $column_count <> $value_count) {
             throw new Exception("Column count ($column_count) does not match values count ($value_count).");
         }
-        
+
         $query .= "VALUES ($value_sql) ";
 
         echo $query;
-        
+
     }
-    
+
     /**
      *  Iterate Result Set
      *
-     *  Returns a {@link MySQL_ResultSet} iteratable object for a query. The $type 
+     *  Returns a {@link MySQL_ResultSet} iteratable object for a query. The $type
      *  parameter indicates the data being iterated should be an object,
      *  a numerically indexed array, an associative array, or an array with
      *  both numeric and associative indexes. Defaults to objects.
@@ -543,14 +543,14 @@ class MySqlDatabase
      *  @param  integer
      *  @return boolean
      */
-    public function iterate($sql, $data_type=MySqlResultSet::DATA_OBJECT) 
+    public function iterate($sql, $data_type=MySqlResultSet::DATA_OBJECT)
     {
         return new MySqlResultSet($sql, $data_type, $this->link);
     }
-    
+
     /**
      *  Load File
-     *  
+     *
      *  Loads the specified filename into an array of lines. Throws an exception
      *  if there is an error.
      *
@@ -562,34 +562,34 @@ class MySqlDatabase
         if (!file_exists($filename)) {
             throw new Exception("File does not exist: $filename");
         }
-        
+
         $file = @file($filename, FILE_IGNORE_NEW_LINES);
-        
+
         if (!$file) {
             throw new Exception("Could not open $filename");
         }
-        
+
         return $file;
     }
 
-    public function query($query) 
+    public function query($query)
     {
         $r = @mysqli_query($query, $this->link);
 
         if (!$r) {
-            throw new Exception("Query Error: " . mysql_error());
+            throw new Exception("Query Error: " . mysqli_error());
         }
-        
+
         return $r;
     }
-    
+
     /**
      *  Quick Query
-     *  
+     *
      *  Executes a MySQL query and returns a boolean value indicating success
      *  or failure. This method will close any resources opened from
      *  SELECT, SHOW, DESCRIBE, or EXPLAIN statements and would not be very
-     *  usefull for those types of queries. This method is used internally for 
+     *  usefull for those types of queries. This method is used internally for
      *  importing SQL scripts.
      *
      *  @param  string
@@ -598,13 +598,13 @@ class MySqlDatabase
     public function quickQuery($query)
     {
         $r = @mysqli_query($query, $this->link);
-        
+
         if (!$r) return false;
         if (is_resource($r)) mysql_free_result($r);
 
         return true;
     }
-    
+
     /**
      *  Update
      *
@@ -619,17 +619,17 @@ class MySqlDatabase
      *  @param  string
      *  @return integer
      */
-    public function update($query) 
+    public function update($query)
     {
         return $this->updateOrDelete($query);
     }
-    
+
     private function updateOrDelete($query)
     {
         $r = $this->query($query);
         return @mysqli_affected_rows($this->link);
     }
-    
+
     /**
      *  Use Database
      *
@@ -639,11 +639,11 @@ class MySqlDatabase
      *  @param  string
      *  @return integer
      */
-    public function useDatabase($database) 
+    public function useDatabase($database)
     {
         if (!@mysqli_select_db($database, $this->link))
         {
-            throw new Exception('Unable to select database: ' . mysql_error($this->link));
+            throw new Exception('Unable to select database: ' . mysqli_error($this->link));
         }
     }
 }
